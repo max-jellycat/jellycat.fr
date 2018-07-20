c53345e296ec7e421ba65c5a0f3ca4b5794dac48
max-jellycat <max@jellycat.fr>
2018-07-20 23:26:03 +0200

    Initial commit :sunglasses:

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+import Other from './components/Other';
+
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
+
